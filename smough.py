2edd4cd32e4d5c21a258752f1df7e851a3cb6fdf
Bradford Smith <me@bradford-smith.com>
2016-02-06 17:57:23 -0600

    Started adding in interface stuff

--- smough.py
+++ smough.py
@@ -1,8 +1,27 @@
 import curses, time
 
+def quit():
+    curses.endwin()
+    sys.exit(0)
+
+def newmap():
+    pass
+
+menu_options = { "Create New Map": newmap, "Exit Smough": quit }
+
 def main():
     screen = curses.initscr()
-    time.sleep(3);
+    curses.noecho()
+    screen.clear()
+    screen.border()
+    indent = 3
+    screen.addstr(1, 1, "Smough v0.1 (pre-alpha)")
+    current_line = 3
+    for key, value in menu_options.iteritems():
+        screen.addstr(current_line, indent, key)
+        current_line += 1
+    screen.refresh()
+    time.sleep(3)
     curses.endwin()
 
 if __name__ == "__main__":
